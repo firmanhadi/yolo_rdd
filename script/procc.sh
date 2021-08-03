0e07fdf985e71e7f041aec8bb773115e1b720af1
zectre <43196730+zectre@users.noreply.github.com>
2021-08-03 19:21:32 +0700

    Update procc.sh

--- script/procc.sh
+++ script/procc.sh
@@ -1,12 +1,13 @@
-python cut.py
-python color.py
-cd selected
+python ./script/cut.py
+python ./script/color.py
+cd output
 mkdir bw
 cp *leveled.png ./bw/
 cd bw
 find . -name '*_leveled.png' -exec bash -c ' mv $0 ${0/\_cropped_leveled/}' {} \;
 for f in *.png;do tesseract -l eng "$f" "$(basename "$f" .png)";done
-#for i in ./*.txt; do echo $(cat $i); done > wew.txt
-#ls ./*.png > filename.txt
-#paste filename.txt wew.txt filename.txt > fin.txt
+for i in ./*leveled.txt; do echo $(cat $i); done > wew.txt
+ls *.png > file.txt
+sed 's/_leveled.png/.png/g' file.txt > filename.txt
+paste filename.txt wew.txt  > /content/tesseractresult.txt
 #awk 'FNR==NR{a[$1]=$2;next} ($1 in a) {print $1,a[$1],$3,$2}' outputyolo.txt outputlatlon.txt > resultmerged.txt
