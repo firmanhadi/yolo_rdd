47ddab1e2a4b40c57fe5e9b6e081404e472c7a7c
zectre <43196730+zectre@users.noreply.github.com>
2021-08-04 19:50:03 +0700

    Update cut.py

--- script/cut.py
+++ script/cut.py
@@ -5,3 +5,11 @@ for png_file in glob('./output/*.jpg'):
     image = Image.open(png_file)
     cropped = image.crop((200, 965, 1700, 1080))
     cropped.save(png_file.replace('.jpg', '_cropped.png'))
+
+#    from PIL import Image
+#from glob import glob
+#import os
+#for png_file in glob('./output/*.jpg'):
+#    image = Image.open(png_file)
+#    cropped = image.crop((458, 105, 1442, 161))
+#    cropped.save(png_file.replace('.jpg', '_cropped.png'))
